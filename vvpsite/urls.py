4ea581fe80b8bc5b84955ad1fc389c3caa1e5268
Partha <parthavbiz@gmail.com>
2021-01-10 14:34:04 -0800

    fixing the ysutra stack image and setting up the root URL

--- vvpsite/urls.py
+++ vvpsite/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', include('mainapp.urls')),
     path('main/', include('mainapp.urls')),
     path('blogs/', include('blogs.urls')),
     path('admin/', admin.site.urls)
