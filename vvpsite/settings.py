26f39d97f0a296b5fac47302270a3a943709706a
Partha <parthavbiz@gmail.com>
2021-02-11 18:17:57 -0800

    database changes

--- vvpsite/settings.py
+++ vvpsite/settings.py
@@ -83,13 +83,31 @@ WSGI_APPLICATION = 'vvpsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'vvp',
+
+        'USER': 'vvp',
+
+        'PASSWORD': 'vedanta750@',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
