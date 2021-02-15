2328d38589c28b1eabcff497f960229a13fa1c40
Partha <parthavbiz@gmail.com>
2021-02-15 12:54:28 -0800

    Added textual gem and reduced spacing between line 1,2 and comments

--- rewards/forms.py
+++ rewards/forms.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 
 class StrokeForm(forms.Form):
     stroke = forms.ChoiceField(label="Stroke", choices=Stroke.choice_labels())
-    category = forms.CharField(label='Line 1', max_length=15, required=True)
-    action = forms.CharField(label='Line 2', max_length=20, required=True)
+    category = forms.CharField(label='Line 1', max_length=25, required=True)
+    action = forms.CharField(label='Line 2', max_length=25, required=True)
     comments = forms.CharField(label='Comments', required=False, max_length=50, widget=forms.Textarea )
     user = forms.CharField(widget=forms.HiddenInput())
     stroker = forms.CharField(label='Your name', max_length=25)
