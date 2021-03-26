801d5d9047463180d89f65a06d3c9400e6746d17
Partha <parthavbiz@gmail.com>
2021-03-25 17:50:55 -0700

    set the form Gift with increased textarea size

--- rewards/admin.py
+++ rewards/admin.py
@@ -6,17 +6,17 @@ admin.site.register(MyStroke)
 admin.site.register(Reward)
 admin.site.register(Coach)
 admin.site.register(StudentProgress)
-admin.site.register(Gift)
+# admin.site.register(Gift)
 admin.site.register(UserGift)
+from django.forms import ModelForm, Textarea
 
-#
-# class GiftAdmin(admin.ModelAdmin):
-#     formfield_overrides = {
-#     models.TextField: {'widget': Textarea(
-#                        attrs={'rows': 10,
-#                               'cols': 100})},
-# }
-#
-#
-# # Register your models here.
-# admin.site.register(Gift, GiftAdmin)
+class GiftAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+    models.TextField: {'widget': Textarea(
+                       attrs={'rows': 10,
+                              'cols': 100})},
+}
+
+
+# Register your models here.
+admin.site.register(Gift, GiftAdmin)
