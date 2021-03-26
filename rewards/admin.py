92c2709a604acbc9ce3f8125bf7604399d6a3c15
Partha <parthavbiz@gmail.com>
2021-03-25 17:43:13 -0700

    Added Gift Admin

--- rewards/admin.py
+++ rewards/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.db import models
 from .models import MyStroke, Reward, Coach, StudentProgress, Gift,UserGift
 
 admin.site.register(MyStroke)
@@ -9,4 +9,14 @@ admin.site.register(StudentProgress)
 admin.site.register(Gift)
 admin.site.register(UserGift)
 
+
+class GiftAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+    models.TextField: {'widget': Textarea(
+                       attrs={'rows': 10,
+                              'cols': 100})},
+}
+
+
 # Register your models here.
+admin.site.register(Gift, GiftAdmin)
