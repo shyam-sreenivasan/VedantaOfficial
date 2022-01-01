7726c6c327d980de76d615d745ed35ec4969a601
Partha <parthavbiz@gmail.com>
2022-01-01 10:35:44 -0800

    no message

--- mainapp/views.py
+++ mainapp/views.py
@@ -46,7 +46,7 @@ def send_email(request):
     if form.is_valid():
         name = form.data['name']
         email = form.data['email']
-        message = "Subject: ".format(form.data['message']) + "\n" + "Content: ".format(form.data['content'])
+        message = "Subject: ".format(form.data['subject']) + "\n" + "Message: ".format(form.data['message'])
 
         subject = \
             'Message from Customer: {} {}'.format(name, email)
