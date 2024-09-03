843491f39cf38e87eddb2bde7c451e449a6061f0
Partha <parthavbiz@gmail.com>
2024-09-03 16:10:22 -0700

    bringing in postgres db

--- vvpsite/settings.py
+++ vvpsite/settings.py
@@ -82,16 +82,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vvpsite.wsgi.application'
 
-DEBUG = True
+DEBUG = False
 
 #Database
 #https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # if DEBUG:
 #
